4631fa1bad978ce8812862000eea99043b097550
newhorizon-tech <alaukikinbox@gmail.com>
2020-11-27 21:00:07 +0530

    Added image upload feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
     has_many :comments, :dependent => :delete_all
     has_many :taggings
     has_many :tags, through: :taggings
+    has_one_attached :image
     def tag_list
       tags.join(', ')
     end
