5c616089527411cedcb271ea5473e1a5c05e8b02
enkog <nkirukaogu@gmail.com>
2020-11-27 14:01:01 +0100

    Finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -40,4 +42,5 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
 end
